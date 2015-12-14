91ede0071c182f34ea8a2dabcdd7dc3bb7bac3d0
Natalya Nedbailo <tygryskuns@gmail.com>
2015-12-14 12:12:56 +0200

    controller added; template changed; form added to template; tcontacts

--- app/code/local/Ainstainer/Contacts/controllers/IndexController.php
+++ app/code/local/Ainstainer/Contacts/controllers/IndexController.php
@@ -2,7 +2,7 @@
 class Ainstainer_Contacts_IndexController extends Mage_Core_Controller_Front_Action {
 	public function indexAction (){		
 		$this->loadLayout();
-		$this->renderLayout(); 
+		$this->renderLayout();
 	}
 	public function hiAction(){
 		echo "Hi";
